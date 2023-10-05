70989b8b1e737717287721c57a9dc3bdb0ad24ad
hamzahcodes <shaikhhamzah93@gmail.com>
2023-10-05 11:55:54 +0530

    added another info

--- firstProgram.java
+++ firstProgram.java
@@ -1,5 +1,6 @@
 public class firstProgram {
     public static void main(String[] args) {
         System.out.println("Salaam Hamzah, This is your Jenkins first push....");
+        System.out.println("we know that jenkins is a CI-CD tool");
     }
 }
